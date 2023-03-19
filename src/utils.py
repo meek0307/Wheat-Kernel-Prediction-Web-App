aaf5349158a2df00725f4e08858b46c3d0e9c98e
Ramneek <ramneekkaur0307@gmail.com>
2023-03-19 14:30:11 +0530

    hyperparameters and tuning

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
 
 from src.exception import CustomException
 
@@ -29,7 +29,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
+            gs = RandomizedSearchCV(model,para,cv=5,verbose=2,n_jobs=-1)
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
